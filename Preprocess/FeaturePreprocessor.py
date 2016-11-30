699141d29b9f282ba6e987cc89b6e7a4c3d033bd
wqlin <wqlin0@gmail.com>
2016-11-30 15:59:08 +0800

    add feature preprocessor

--- Preprocess/FeaturePreprocessor.py
+++ Preprocess/FeaturePreprocessor.py
@@ -1,5 +1,5 @@
-#coding: utf8
-from .Utils import delete_dir_and_makedir
+# coding: utf8
+from Preprocess.Utils import delete_dir_and_makedir, base
 import pandas as pd
 from os import listdir, makedirs
 from os.path import join
@@ -8,7 +8,6 @@ from os.path import join
 class FeatureExtractor(object):
     """
     从已经完成缺失值填充的文件中进行特征提取
-    主要特征包括前一天的值，前三天均值，前七天均值
     """
 
     def __init__(self, src_dir, target_dir):
@@ -29,7 +28,7 @@ class FeatureExtractor(object):
         :return: 不提取最前面7天的 dataFrame
         """
         rolling_df = pd.rolling_mean(df, window=window).shift(shift)
-        return rolling_df[7:]
+        return rolling_df[90:]
 
     def run(self):
         for dir_path in listdir(self.src_dir):
@@ -40,25 +39,35 @@ class FeatureExtractor(object):
                 df = pd.read_csv(join(src_dir, file))
                 if len(df) > 0:
                     df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
-                    offset = pd.Timedelta(days=7)
+                    offset = pd.Timedelta(days=90)
                     date_range = pd.date_range((df['date'].min() + offset).date(),
                                                df['date'].max().date())
                     averin1 = self.rolling_mean(df['aver'], 1)
-                    averin3 = self.rolling_mean(df['aver'], 3)
                     averin7 = self.rolling_mean(df['aver'], 7)
-                    assert len(averin1) == len(averin3) == len(averin7) == len(df['aver'][7:])
+                    averin15 = self.rolling_mean(df['aver'], 15)
+                    averin30 = self.rolling_mean(df['aver'], 30)
+                    averin90 = self.rolling_mean(df['aver'], 90)
+
+                    assert len(averin1) == len(averin30) == len(averin7) == len(averin15) == len(averin90) == len(
+                        df['aver'][90:])
                     target_df = pd.DataFrame({
                         'province': df['province'][0],
                         'market': df['market'][0],
                         'type': df['type'][0],
-                        'type_mapping': df['type_mapping'][0],
+                        'key': df['key'][0],
                         'date': date_range,
-                        'average': df['average'][7:],
+                        'aver': df['aver'][90:],
                         'averin1': averin1,
-                        'averin3': averin3,
-                        'averin7': averin7
+                        'averin7': averin7,
+                        'averin15': averin15,
+                        'averin30': averin30,
+                        'averin90': averin90
                     })
                     target_df.to_csv(join(target_dir, file), header=True, index=False,
                                      columns=['province', 'market', 'type', 'key', 'date', 'averin1',
-                                              'averin3', 'averin7', 'aver'])
+                                              'averin7', 'averin15', 'averin30', 'averin90', 'aver'])
 
+
+if __name__ == '__main__':
+    feature_extractor = FeatureExtractor(join(base, 'sortImputation'), join(base, 'sortFeature'))
+    feature_extractor.run()
