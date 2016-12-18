439bdbeeecf519c195b932608d52e3bdd6066deb
wqlin <wqlin0@gmail.com>
2016-12-18 19:08:25 +0800

    更新文件名

--- forecast/GBDT.py
+++ forecast/GBDT.py
@@ -4,152 +4,165 @@ from os.path import join
 from Preprocess.Utils import base, delete_dir_and_makedir, merge_result, log, log_file
 import numpy as np
 import pandas as pd
-import datetime
-from sklearn.feature_extraction import DictVectorizer
-import re
-from os import makedirs
-from os import listdir
+import logging
+from os import makedirs, listdir
+# from sklearn.model_selection import GridSearchCV
+from sklearn.grid_search import GridSearchCV
 from os.path import exists
 from datetime import datetime
+from sklearn.utils import shuffle
 
 
 class GBDT(object):
-    def __init__(self, feature_dir_path, sort_result_dir):
+    def __init__(self, feature_dir,
+                 sort_result_dir,
+                 sort_dir):
         """
-        :param feature_dir_path: 分割后带有 feature 的文件夹
-        :param sort_result_dir:　由结果分割后的文件夹
+        :param feature_dir: 存放特征的文件夹
+        :param sort_result_dir: 由 product_market 分割的文件夹
+        :param sort_dir: 由 farming 分割的文件夹
         """
-        self.feature_dir_path = feature_dir_path
+        self.feature_dir = feature_dir
         self.sort_result_dir = sort_result_dir
+        self.sort_dir = sort_dir
 
-    def onehot_encode(self, train_csv_file, predict_csv_file):
+    def predict2file(self, predict_values, res_csv_file):
         """
-        :param train_csv_file: 需要进行独热编码的训练 csv 文件
-        :param predict_csv_file: 需要进行独热编码的预测用 csv 文件
-        :return: 进行热编码后处理的 Xtrain,ytrain,Xpredict
-        xpredict 还需要三个特征
-        TODO 提取正确的值
+        将预测后的值写入文件
+        :param predict_values: 预测值的均值
+        :param res_file: 保存结果文件
+        :return:
         """
-        train_df = pd.read_csv(train_csv_file, header=0,
-                               names=['province', 'market', 'type', 'key', 'date', 'averin1', 'averin3', 'averin7',
-                                      'aver'])
-        # len(df.columns) 返回 dataframe 的长度
-        province_list = train_df['province'].unique()
-        market_list = train_df['market'].unique()
-        type_list = train_df['type'].unique()
-        key_list = train_df['key'].unique()
-        province_list = [item for item in map(lambda v: {'province': v}, province_list)]
-        market_list = [item for item in map(lambda v: {'market': v}, market_list)]
-        type_list = [item for item in map(lambda v: {'type': v}, type_list)]
-        key_list = [item for item in map(lambda v: {'key': v}, key_list)]
+        df = pd.DataFrame({
+            'date': pd.date_range('20160701', '20160731'),
+            'aver': predict_values
+        })
+        df.to_csv(res_csv_file, index=False, header=False,
+                  columns=['date', 'aver'])
 
-        onehot_encoder = DictVectorizer()
+    def predict(self, sort_csv_file,
+                sort_result_file,
+                feature_csv_file,
+                res_csv_file):
+        sort_df = pd.read_csv(sort_csv_file)
+        if sort_df['aver'].std() == 0:  # 如果方差为零，就不使用 GBDT
+            predict_values = sort_df['aver'].mean() * np.ones([1, 31])
+            print(sort_csv_file)
+            self.predict2file(predict_values[0], res_csv_file)
+        else:
+            self.GBDTpredict(feature_csv_file, sort_result_file, res_csv_file)
 
-        province_onehot = onehot_encoder.fit_transform(province_list).toarray()
-        province_map = {}
-        for province, onehot_code in zip(province_list, province_onehot):
-            province_map[province['province']] = onehot_code
-
-        market_onehot = onehot_encoder.fit_transform(market_list).toarray()
-        market_map = {}
-        for market, onehot_code in zip(market_list, market_onehot):
-            market_map[market['market']] = onehot_code
-
-        type_onehot = onehot_encoder.fit_transform(type_list).toarray()
-        type_map = {}
-        for Type, onehot_code in zip(type_list, type_onehot):
-            type_map[Type['type']] = onehot_code
-
-        key_onehot = onehot_encoder.fit_transform(key_list).toarray()
-        key_map = {}
-        for key, onehot_code in zip(key_list, key_onehot):
-            key_map[key['key']] = onehot_code
-
+    def GBDTpredict(self, train_csv_file, predict_csv_file,
+                    res_csv_file):
+        print('begin GBDT training')
         Xtrain, ytrain = [], []
+        train_df = pd.read_csv(train_csv_file)
+        price_values = np.array(train_df['aver'].tolist())
+        train_df=shuffle(train_df)
         for item in train_df.itertuples(index=False):
-            onehot_code = np.concatenate((province_map[item[0]], market_map[item[1]], type_map[item[2]],
-                                          key_map[item[3]])).tolist()
             date = datetime.strptime(item[4], '%Y-%m-%d')
+            feature = [date.year, date.month, date.day, item[5], item[6], item[7], item[8], item[9]]
+            Xtrain.append(feature)
+            ytrain.append([item[-1]])
 
-            onehot_code = onehot_code + [date.timestamp(), item[5], item[6], item[7]]
-            Xtrain.append(onehot_code)
-            ytrain.append([item[8]])
+        Xtrain = np.array(Xtrain)
+        ytrain = np.array(ytrain)
+        param_test1 = [
+            {'n_estimators': [i for i in range(100, 701, 100)], 'learning_rate': [i / 100 for i in range(10, 101, 10)]}]
+        params = {'max_depth': 4, 'min_samples_split': 20, 'min_samples_leaf': 5,
+                  'loss': 'ls', 'max_features': 'sqrt',
+                  'subsample': 0.8,
+                  'random_state': 10}
+        gsearch = GridSearchCV(estimator=GradientBoostingRegressor(**params),
+                               param_grid=param_test1, n_jobs=4, iid=False, cv=10)
+        gsearch.fit(Xtrain, np.ravel(ytrain))
+        best_params1 = gsearch.best_params_
+        print('best_params1')
+        print(best_params1)
+        print(gsearch.best_score_)
 
-        predict_df = pd.read_csv(predict_csv_file, header=0,
-                                 names=['province', 'market', 'type', 'key', 'date'])
-        print(predict_df['market'][0])
-        price_values = train_df.query("market=='%s'" % str(predict_df['market'][0]))['aver'].tolist()
+        params['n_estimators'] = best_params1['n_estimators']
+        params['learning_rate'] = best_params1['learning_rate']
+        # 更新参数
 
-        Xpredict = []
+        del (params['max_depth'])
+        del (params['min_samples_split'])
+
+        param_test2 = [{'max_depth': [i for i in range(2, 7, 1)],
+                        'min_samples_split': [i for i in range(10, 251, 10)]}]
+        gsearch = GridSearchCV(estimator=GradientBoostingRegressor(**params),
+                               param_grid=param_test2)
+        gsearch.fit(Xtrain, np.ravel(ytrain))
+        best_params2 = gsearch.best_params_
+        print('best_params2')
+        print(best_params2)
+        print(gsearch.best_score_)
+
+        params['max_depth'] = best_params2['max_depth']
+        params['min_samples_split'] = best_params2['min_samples_split']
+
+        param_test3 = [{'min_samples_split': [i for i in range(10, 251, 10)],
+                        'min_samples_leaf': [i for i in range(10, 151, 10)]}]
+        gsearch = GridSearchCV(estimator=GradientBoostingRegressor(**params),
+                               param_grid=param_test3)
+        gsearch.fit(Xtrain, np.ravel(ytrain))
+        best_params3 = gsearch.best_params_
+        print('best_params3')
+        print(best_params3)
+        print(gsearch.best_score_)
+
+        params['min_samples_split'] = best_params3['min_samples_split']
+        params['min_samples_leaf'] = best_params3['min_samples_leaf']
+        print('params:')
+        print(params)
+        log(log_file, str(params) + '\n')
+        # regressor = GradientBoostingRegressor(**params)
+        # regressor.fit(Xtrain, np.ravel(ytrain))
+
+
+        predict_price = []
+        predict_df = pd.read_csv(predict_csv_file)
         for item in predict_df.itertuples(index=False):
-            onehot_code = np.concatenate((province_map[item[0]], market_map[item[1]], type_map[item[2]],
-                                          key_map[item[3]])).tolist()
-            date = datetime.strptime(item[4], '%Y-%m-%d')
-            onehot_code.append(date.timestamp())
-            Xpredict.append(onehot_code)
-
-        return np.array(Xtrain), np.array(ytrain), np.array(Xpredict), np.array(price_values)
-
-    def fit(self, Xtrain, ytrain, params):
-        """
-        :param Xtrain: 特征集
-        :param ytrain: 结果集
-        :param params: 参数
-        :return:　训练模型
-        """
-        regressor = GradientBoostingRegressor(**params)
-        regressor.fit(Xtrain, ytrain)
-        return regressor
-
-    def predict(self, train_csv_file, predict_csv_file, res_file):
-        Xtrain, ytrain, XPredict, price_values = self.onehot_encode(train_csv_file, predict_csv_file)
-        params = {'n_estimators': 500, 'max_depth': 8, 'min_samples_split': 2,
-                  'learning_rate': 0.01, 'loss': 'ls'}
-        regressor = self.fit(Xtrain, np.ravel(ytrain), params)
-        date_range = pd.date_range('20160701', '20160731')
-        predict_df = pd.DataFrame({
-            'date': date_range,
-        }, index=date_range)
-        values = []
-        for i in range(0, 31):
-            feature = XPredict[i]
             averin1 = price_values[-1:].mean()
-            averin3 = price_values[-3:].mean()
             averin7 = price_values[-7:].mean()
-            features = np.append(feature, [averin1, averin3, averin7]).reshape(1, -1)
-            predict_value = regressor.predict(features)[0]
-            assert float(predict_value) >= 0.0
-            assert float(predict_value) <= 99999
-            price_values = np.append(price_values, [predict_value])
-            # ytrain = np.append(ytrain, [predict_value])
-            values.append(predict_value)
-        predict_df['aver'] = values
-        predict_df.to_csv(res_file, index=False, header=False,
-                          columns=['date', 'aver'])
+            averin15 = price_values[-15:].mean()
+            averin30 = price_values[-30:].mean()
+            averin90 = price_values[-60:].mean()
+            date = datetime.strptime(item[-1], '%Y-%m-%d')
+            predict_feature = np.array([date.year, date.month, date.day, averin1, averin7, averin15,
+                                        averin30, averin90]).reshape(1, -1)
+            price = gsearch.predict(predict_feature)[0]
+            predict_price.append(price)
+            price_values = np.append(price_values, [price])
+        print(np.array(predict_price).reshape(1, -1))
+        self.predict2file(np.array(predict_price).reshape(1, -1)[0], res_csv_file)
 
     def run(self, forecast_dir):
         delete_dir_and_makedir(forecast_dir)
         for dir_path in listdir(self.sort_result_dir):
-            forecast_dir_path = join(forecast_dir, dir_path)
-            if not exists(forecast_dir_path):
-                makedirs(forecast_dir_path)
+            if not exists(join(forecast_dir, dir_path)):
+                makedirs(join(forecast_dir, dir_path))
             for file in listdir(join(self.sort_result_dir, dir_path)):
-                file_name = re.findall(r'[a-zA-Z0-9].*', file)[0]
-                train_csv_file = join(self.feature_dir_path, file_name)  # 训练文件路径
-                predict_csv_file = join(self.sort_result_dir, dir_path, file)  # 需要预测文件路径
-                res_file = join(forecast_dir_path, file)  # 保存结果文件路径
-                if not exists(res_file):
-                    log(log_file, predict_csv_file)
-                    self.predict(train_csv_file, predict_csv_file, res_file)
+                sort_csv_file = join(self.sort_dir, dir_path, file)
+                sort_result_csv_file = join(self.sort_result_dir, dir_path, file)
+                feature_csv_file = join(self.feature_dir, dir_path, file)
+                res_csv_file = join(forecast_dir, dir_path, file)
+                if not exists(res_csv_file):
+                    log(log_file, res_csv_file)
+                    self.predict(sort_csv_file, sort_result_csv_file,
+                                 feature_csv_file, res_csv_file)
 
 
 if __name__ == '__main__':
-    import warnings
-
-    with warnings.catch_warnings():
-        forecast_dir = join(base, 'forecast')
-        sort_result = join(base, 'sort_result')
-        warnings.simplefilter("error")
-        gbdt = GBDT(join(base, 'farmingFeature'), sort_result)
+    sort_dir = join(base, 'sort')
+    sort_result_dir = join(base, 'sort_result')
+    forecast_dir = join(base, 'myforecast')
+    featrue_dir = join(base, 'sortFeature')
+    gbdt = GBDT(featrue_dir, sort_result_dir,
+                sort_dir)
+    logging.basicConfig(filename='exception.log', level=logging.DEBUG)
+    try:
         gbdt.run(forecast_dir)
-        merge_result(forecast_dir, sort_result, join(base, 'fuck.csv'))
+        merge_result(forecast_dir, sort_result_dir, 'myfuckgdbt.csv')
+    except:
+        logging.exception("Opps")
